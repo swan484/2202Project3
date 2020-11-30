9f07da6f187513cc7d7dbfbd039db715635b4b61
swan484 <swan484@uwo.ca>
2020-11-29 20:39:57 -0500

    index

---
+++ index.js
@@ -0,0 +1,4 @@
+//Read data in JSON and use classes defined to perform analysis on given data. Write output in JSON file.
+//Output file should contain list of JSON objects with
+// "time interval" and "revenue" property
+
