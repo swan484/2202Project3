5a93dc16ddaa6b65547c0002485af5783e8d9944
swan484 <swan484@uwo.ca>
2020-11-30 12:36:42 -0500

    not sure how to make this class

--- BudgetAnalysis.js
+++ BudgetAnalysis.js
@@ -2,8 +2,9 @@ const BudgetItem = require('./BudgetItem.js');
 const Expense = require('./Expense.js');
 const Income = require('./Income.js');
 
-class BudgetAnalysis{
+class BudgetAnalysis { //bro idk what to do for this class
     constructor(bItems){
+
         var __BudgetItems__ = new Array();
         __BudgetItems__ = bItems;
     }
@@ -11,6 +12,9 @@ class BudgetAnalysis{
         __BudgetItems__.push(bItem)
     }
     getMontlyBudgetItems(month){
+        monthlyBudgetItems = new Array();
+        
+
         
     }
 
