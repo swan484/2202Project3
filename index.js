fcecfb3ffc0a9729816d5a77dd421ca2fb5854d4
swan484 <swan484@uwo.ca>
2020-12-01 22:19:10 -0500

    draft 1

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//Read data in JSON and use classes defined to perform analysis on given data. Write output in JSON file.
-//Output file should contain list of JSON objects with
-// "time interval" and "revenue" property
-
 let BudgetAnalysis = require("./BudgetAnalysis");
 const fs = require("fs")
 let Objects = require("./Objects.json");
